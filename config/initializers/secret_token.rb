d7e36ab751cc1a3cc2a21c696ef4a35d96181be6
Arpit Patel <userapt567@gmail.com>
2013-04-05 16:09:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mposts::Application.config.secret_token = '54fbc267f25dc4e607d28822ff0248c08d1d12b74a1ffb56928162fa725643a8550cd276bfe8db350ae37a3a07136c7098cca0af5614e6c52ab37c933da0317c'
